3366b23c516572121f072b39db97d65a012e6bab
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-09-24 03:04:49 +0300

    feat: Working with files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from './Post';
 import './styles/styles.css';
+import json from './assets/json.json';
+import logo from './assets/webpack-logo.png';
 
-const post = new Post('Post title');
+const post = new Post('Post title', logo);
 
 console.log('Post to string', post.toString());
+console.log('JSON: ', json);
